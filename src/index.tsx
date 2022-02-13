07a47a32b7083be45f9f55d405618ccdc50cd32a
Joao Pedro Noronha <joaopdrons@gmail.com>
2022-02-13 16:34:24 -0300

    modal and forms

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'DEV',
+          amount: 6000,
+          createdAt: new Date('2022-02-13')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1000,
+          createdAt: new Date('2022-02-14')
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
-      return {
-        id: 1, 
-        title: 'Transaction 1',
-        amout: 400,
-        type: 'deposit',
-        category: 'Food',
-        createdAt: new Date(),
-      }
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
